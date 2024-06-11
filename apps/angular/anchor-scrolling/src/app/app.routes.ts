ecd7537b5d2c90dbd68264b8b4746e5d78c37f6e
googray <shat.rom07@gmail.com>
2024-06-11 21:30:03 +0200

    feat: new app implement

--- apps/angular/anchor-scrolling/src/app/app.routes.ts
+++ apps/angular/anchor-scrolling/src/app/app.routes.ts
@@ -1,4 +1,5 @@
-import { Route } from '@angular/router';
+import { ApplicationConfig } from '@angular/core';
+import { Route, provideRouter } from '@angular/router';
 import { FooComponent } from './foo.component';
 import { HomeComponent } from './home.component';
 
@@ -7,3 +8,7 @@ export const appRoutes: Route[] = [
   { path: 'foo', component: FooComponent },
   { path: '**', redirectTo: 'home' },
 ];
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(appRoutes)],
+};
--- apps/angular/anchor-scrolling/src/app/foo.component.ts
+++ apps/angular/anchor-scrolling/src/app/foo.component.ts
@@ -1,15 +1,31 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NavButtonComponent } from './nav-button.component';
 
 @Component({
   standalone: true,
-  imports: [NavButtonComponent],
+  imports: [NavButtonComponent, RouterLink],
   selector: 'app-foo',
   template: `
     Welcome to foo page
-    <nav-button href="home" class="fixed left-1/2 top-3">Home Page</nav-button>
-    <div class="h-screen bg-blue-200">section 1</div>
-    <div class="h-screen bg-red-200">section 2</div>
+    <nav-button routerLink="/home" class="fixed left-1/2 top-3">
+      Home Page
+    </nav-button>
+    <div id="section1" class="h-screen bg-blue-200">
+      <nav-button (click)="onClickScroll('section2')">section 2</nav-button>
+    </div>
+    <div id="section2" class="h-screen bg-red-200">
+      <nav-button (click)="onClickScroll('section1')">section 1</nav-button>
+    </div>
   `,
 })
-export class FooComponent {}
+export class FooComponent {
+  constructor(private viewportScroller: ViewportScroller) {}
+
+  onClickScroll(elementId: string): void {
+    if (elementId) {
+      this.viewportScroller.scrollToAnchor(elementId);
+    }
+  }
+}
