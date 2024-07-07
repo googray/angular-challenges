2e0a4d0015aeef2473a5f903131e058244b0d77a
googray <shat.rom07@gmail.com>
2024-07-07 21:11:38 +0200

    fix: router_input_v1 - refactorcode

--- apps/angular/router-input/src/main.ts
+++ apps/angular/router-input/src/main.ts
@@ -1,7 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
+import { appRoutes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) =>
-  console.error(err),
-);
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err),
+// );
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes, withComponentInputBinding())],
+});
